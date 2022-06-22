0f66a35e49d619a83853d3224c4f77a412eb9231
Pradnyesh7 <107871205+Pradnyesh7@users.noreply.github.com>
2022-06-22 18:41:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var Box; 
+function setup(){
+    createCanvas(400,400)
+
+    Box = createSprite(200,200,40,40)
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+function draw (){
+
+background(30)
+
+    drawSprites()
+
+
+
+
+if (mouseWentDown("leftButton")) {
+background("rgb");
+}
+if (mouseWentDown("rightButton")) {
+    background("green");
+    }
+
+
+}
